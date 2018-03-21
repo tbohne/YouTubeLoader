7f3abf4a17908097a381dfbd740598dacb1ca4ef
tbohne <tbohne@uos.de>
2018-03-21 21:49:26 +0100

    implemented several url manipulations to be able to start download

--- loader.js
+++ loader.js
@@ -5,6 +5,37 @@ function extract_title(http_response) {
     return http_response.substring(start_idx, end_idx);
 };
 
+function remove_itag_params(url) {
+    var itag_idx = url.search("itag=");
+    var substring = url.substring(itag_idx, "&");
+    return substring;
+};
+
+function encode_utf8(s) {
+    return unescape(encodeURIComponent(s));
+};
+
+function extract_url(http_response) {
+
+    var str = "\"url_encoded_fmt_stream_map\":";
+
+    var start_idx = http_response.search(str) + str.length + 1;
+    var url_start_idx = http_response.indexOf("http", start_idx);
+
+    var append_to_url = "";
+    var url_meta_length = 4;
+
+    if (start_idx != url_start_idx - url_meta_length) {
+        append_to_url += "&";
+        append_to_url += http_response.substring(start_idx, url_start_idx - start_idx);
+    }
+    var end_idx = http_response.indexOf(",", url_start_idx);
+    var url = http_response.substring(url_start_idx, end_idx);
+    url += append_to_url;
+    url = encode_utf8(url);
+    url = remove_itag_params(url);
+};
+
 function init_url() {
 
     var url_input = document.getElementById("url_input");
@@ -14,11 +45,12 @@ function init_url() {
         if (event.keyCode === 13) {
 
             xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange=function() {
+            xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                     var http_response = xmlhttp.responseText;
                     var title = extract_title(http_response);
                     console.log(title);
+                    extract_url(http_response);
                 }
             }
             var url = "https://crossorigin.me/" + url_input.value;
