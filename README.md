2c891854cb7bf54c829b6c4673617ed3613d6125
tbohne <tbohne@uos.de>
2018-03-22 23:38:11 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+YouTubeLoader
+=====================================================
+Simple web application to download videos from YouTube.
+
+### DEPENDENCIES
+- **serve-static** --> npm install serve-static
+- **connect** --> npm install connect
+
+### USAGE
+```
+$ node server.js
+```
+Navigate to localhost:8080 to use the app.
--- server.js
+++ server.js
@@ -2,10 +2,6 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
+connect().use(serveStatic(__dirname)).listen(8080, function() {
     console.log('Server running on 8080...');
 });
-
-// Run with Node.js
-// $ node server.js
-// You can now go to http://localhost:8080/yourfile.html
